29abb4ae9b60a43c3c7082e1e9b8673254409ecd
Shivansh Gupta <68062195+Shivansh770@users.noreply.github.com>
2023-09-18 16:17:07 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
+import "hardhat/console.sol";
 
-contract DegenToken is ERC20, Ownable {
-
+contract Degen is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("Degen", "DGN") {}
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function Decimals() public pure returns (uint8) { 
+        return 0;
+    }
+
+    function GetBalance() external view returns (uint256) { 
+            return this.balanceOf(msg.sender);
+    }
+
+    function TransferTokens(address receiver, uint256 _value) external { 
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens"); 
+        approve(msg.sender, _value); transferFrom(msg.sender, receiver, _value);
+    }
+    function BurnSomeTokens (uint256 _value) public view{ 
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
     }
 }
